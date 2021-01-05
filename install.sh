c2c52ebdb05f41eb53c8b474c61e7ad2190040f9
Stefan Kluth <skluth@mpp.mpg.de>
2021-01-05 15:18:06 +0100

    clean out commented section

--- install.sh
+++ install.sh
@@ -14,16 +14,6 @@ dpkg-reconfigure libdvd-pkg
 # Install config mods
 ./install-config-mods.sh
 
-#systemctl stop vdr.service
-#cp 00-vdr.conf /etc/vdr/conf.d/
-#cp setup.conf /var/lib/vdr/setup.conf
-#cp S90.lifeguard /etc/vdr/shutdown-hooks/
-#cp S95.acpi-wakeup /etc/vdr/shutdown-hooks/
-#ln -s /etc/vdr/shutdown-hooks/S90.lifeguard /usr/share/vdr/shutdown-hooks/
-#ln -s /etc/vdr/shutdown-hooks/S95.acpi-wakeup /usr/share/vdr/shutdown-hooks/
-#cp vdr /etc/default/
-#cp ngene_18.fw /lib/firmware/
-
 # Add user and configure
 adduser tvglotzer
 echo tvglotzer:ninahagen | chpasswd
